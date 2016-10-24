d2f339ee78f44f1624347433c744652debe00299
Julien MIGUEL-PAREDES <jumiguel@e1r5p15.42.fr>
2016-10-24 18:52:09 +0200

    yolosalefdp

--- src/get_ynext.c
+++ src/get_ynext.c
@@ -6,7 +6,7 @@
 /*   By: aaudiber <aaudiber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/24 17:00:10 by aaudiber          #+#    #+#             */
-/*   Updated: 2016/10/24 17:19:56 by aaudiber         ###   ########.fr       */
+/*   Updated: 2016/10/24 18:45:06 by jumiguel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,33 @@ t_crd		*get_ynext(t_crd *map)
 	t_crd *tmp;
 
 	ret = map;
+	map = map->next;
 	tmp = map->next;
 	while (map)
 	{
-		while ((tmp->x != map->x) && (tmp->y == (map->y + 1)))
+		while (tmp && (tmp->x != map->x) && (tmp->y != (map->y + 1)))
 			tmp = tmp->next;
-		if (!tmp)
+		if (!tmp || !map->next)
 		{
 			map->ynext = NULL;
 			tmp = map->next;
 		}
 		else
+		{
+			ft_putstr("curr --> x = ");
+			ft_putnbr(map->x);
+			ft_putstr(", y = ");
+			ft_putnbr(map->y);
+			ft_putstr(" || ynext --> x = ");
+			ft_putnbr(tmp->x);
+			ft_putstr(", y = ");
+			ft_putnbr(tmp->y);
+			ft_putchar('\n');
 			map->ynext = tmp;
+		}
 		map = map->next;
+		if (map->next)
+			tmp = map->next;
 	}
 	return (ret);
 }
