f737280b61e6eaa8065c2daa2c96518e97d1bcd4
Arthur AUDIBERT <aaudiber@e1r2p22.42.fr>
2016-11-01 17:33:04 +0100

    efrve

---
+++ src/get_ynext.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_ynext.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaudiber <aaudiber@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/24 17:00:10 by aaudiber          #+#    #+#             */
+/*   Updated: 2016/10/31 20:20:31 by aaudiber         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/FdF.h"
+
+t_crd		*get_ynext(t_crd *map)
+{
+	t_crd *ret;
+	t_crd *tmp;
+
+	tmp = map;
+	ret = map->next;
+	while (tmp)
+	{
+		ret = tmp->next;
+		while (ret)
+		{
+			if (ret && (ret->y == (tmp->y + 1)) && tmp->x == ret->x)
+			{
+				tmp->ynext = ret;
+				break ;
+			}
+			ret = ret->next;
+		}
+		tmp = tmp->next;
+	}
+	return (map);
+}
